2e5fcbd1910ca11c7ebf69c53abffa6f62495e65
Alyona <alymkan23@gmail.comgit config --global user.name >
2025-03-13 15:21:18 +0600

    My commit

---
+++ lesson_4/study_github.py
@@ -0,0 +1,8 @@
+class Kadam_Tur:
+    def __init__(self, name, location):
+        self.name = name
+        self.location = location
+
+
+    def func(self):
+        print(f'{self.name:<20} {self.location:>5}')
---
+++ main.py
@@ -0,0 +1,4 @@
+from lesson_4.study_github import Kadam_Tur
+
+first_tour = Kadam_Tur('Кадамжай', 'Баткенская область')
+first_tour.func()
