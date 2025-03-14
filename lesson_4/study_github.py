10c0ffe8ab3477fa30566d685660487febcf08d0
Alyona <alymkan23@gmail.comgit config --global user.name >
2025-03-14 14:44:16 +0600

    Added class Tourist

--- lesson_4/study_github.py
+++ lesson_4/study_github.py
@@ -6,3 +6,15 @@ class Kadam_Tur:
 
     def func(self):
         print(f'{self.name:<20} {self.location:>5}')
+
+
+class Tourist:
+    def __init__(self, name, contact_num, start_date=None):
+        self.name = name
+        self.contact_num = contact_num
+        self.start_date = start_date
+
+
+    def calculate_cost(self):
+        return 'You have the membership card to redeem'
+
